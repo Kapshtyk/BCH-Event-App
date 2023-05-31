0cebd984e07985d2db007e64be8563f3139b2b6a
Alex Bond <s2300120@edu.bc.fi>
2023-05-31 11:07:49 +0300

    Helsinki API first data output added

--- summer-frontend/src/components/Helsinki.tsx
+++ summer-frontend/src/components/Helsinki.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import classes from './Helsinki.module.css';
 
 const Helsinki = () => {
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        axios.get('https://api.hel.fi/linkedevents/v1/event/')
+            .then(response => {
+                setData(response.data.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+    console.log(data);
     return (
         <div className={classes.helsinki}>
-            <p>Helsinki Info</p>
+            {data.length ? (
+                <ul>
+                    {data.map(event => (
+                        <li key={event.id}>
+                            <a href={event.info_url.fi}>{event.name.fi}</a>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     );
 };
